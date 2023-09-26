f599829474c4875074e8edbcd8449e93e58ca7a2
MMK <mwangim4786@gmail.com>
2023-09-26 23:31:49 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -5,7 +5,7 @@
 #include <stddef.h>
 
 void print_array(const int *array, size_t size);
-/*void print_list(const listint_t *list);*/
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int* a, int* b);
 
