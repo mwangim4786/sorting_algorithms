6cb0892da0a1bab37caee4244610ee9c9a514a77
MMK <mwangim4786@gmail.com>
2023-09-27 00:12:31 +0300

    Check Betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,13 +5,13 @@
  * @b: second array
  * Return:void
  */
-void swap(int* a, int* b)
+void swap(int *a, int *b)
 {
-    int temp;
+        int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+        temp = *a;
+        *a = *b;
+        *b = temp;
 }
 
 
@@ -24,25 +24,25 @@ void swap(int* a, int* b)
  */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    bool swapped;
+        unsigned int i, j;
+        bool swapped;
 
-    if (array == NULL || size < 2)
-        return;
+        if (array == NULL || size < 2)
+                return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = false;
-        for (j = 0; j < size - i - 1; j++)
+        for (i = 0; i < size - 1; i++)
         {
-            if (array[j] > array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);
-                swapped = true;
-            }
+                swapped = false;
+                for (j = 0; j < size - i - 1; j++)
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                                swap(&array[j], &array[j + 1]);
+                                print_array(array, size);
+                                swapped = true;
+                        }
+                }
+                if (swapped == false)
+                        break;
         }
-        if (swapped == false)
-            break;
-    }
 }
