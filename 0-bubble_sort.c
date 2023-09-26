d85c56a64302267ac8058636b6f0b78ebc078159
MMK <mwangim4786@gmail.com>
2023-09-27 00:00:54 +0300

    Corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,22 @@
 #include "sort.h"
 /**
+ * swap - function to swap array positions
+ * @a: first array
+ * @b: second array
+ * Return:void
+ */
+void swap(int* a, int* b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+
+/**
  * bubble_sort - sorts array
  * @array: array to be sorted
  * @size: size of the array
@@ -9,6 +26,10 @@ void bubble_sort(int *array, size_t size)
 {
     unsigned int i, j;
     bool swapped;
+
+    if (array == NULL || size < 2)
+        return;
+
     for (i = 0; i < size - 1; i++)
     {
         swapped = false;
@@ -17,23 +38,11 @@ void bubble_sort(int *array, size_t size)
             if (array[j] > array[j + 1])
             {
                 swap(&array[j], &array[j + 1]);
+                print_array(array, size);
                 swapped = true;
             }
         }
         if (swapped == false)
             break;
     }
-}
-
-/**
- * swap - function to swap array positions
- * @a: first array
- * @b: second array
- * Return:void
- */
-void swap(int* a, int* b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
 }
